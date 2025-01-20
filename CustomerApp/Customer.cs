e78da1e64764d3e70b787d467e00f19a701bf7a1
Abubaker Naji <abubaker.fg@gmail.com>
2025-01-20 16:57:55 +0800

    added the second test

---
+++ CustomerApp/Customer.cs
@@ -0,0 +1,8 @@
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public double ODLimit { get; set; }
+}
---
+++ CustomerApp/Program.cs
@@ -0,0 +1,13 @@
+﻿List<Customer> customers = new List<Customer>{
+    new Customer{Id=1, FirstName="John", LastName="Doe", ODLimit=1000},
+    new Customer{Id=2, FirstName="Jane", LastName="Doe", ODLimit=2000},
+    new Customer{Id=3, FirstName="Jack", LastName="Doe", ODLimit=3000},
+    new Customer{Id=4, FirstName="Jill", LastName="Doe", ODLimit=4000},
+    new Customer{Id=5, FirstName="Joe", LastName="Doe", ODLimit=5000},
+    
+};
+foreach (var customer in customers)
+{
+    Console.WriteLine($"Id: {customer.Id}, Name: {customer.FirstName}, {customer.LastName}, OD Limit: {customer.ODLimit}"); 
+}
+
