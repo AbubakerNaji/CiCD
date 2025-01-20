8bea6d72a917d70962478d38ad1b64cdcecba319
Abubaker Naji <abubaker.fg@gmail.com>
2025-01-20 15:58:01 +0800

    first commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World this is abubaker!");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"The value of i is {i}");
+}
