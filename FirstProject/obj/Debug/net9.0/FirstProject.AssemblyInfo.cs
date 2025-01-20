3e49acfa30451f7582b6a621cb94e3be633a125d
Abubaker Naji <abubaker.fg@gmail.com>
2025-01-20 16:26:32 +0800

    pushed

--- FirstProject/obj/Debug/net9.0/FirstProject.AssemblyInfo.cs
+++ FirstProject/obj/Debug/net9.0/FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bea6d72a917d70962478d38ad1b64cdcecba319")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
